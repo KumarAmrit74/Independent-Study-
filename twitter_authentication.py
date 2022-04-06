17d518d951d4518ac46cb3e988546e4d573192bb
movvahimadeep <51871829+movvahimadeep@users.noreply.github.com>
2022-04-05 23:09:34 -0400

    Push 2
    additional files added

---
+++ twitter_authentication.py
@@ -0,0 +1,43 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "id": "b25e0f7d",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "bearer_token=\"AAAAAAAAAAAAAAAAAAAAAC7TZAEAAAAA9T1QprO8KiiOvxFlwtvqh%2Fartto%3DMn9HAhPhvysEfuyeKUfMjEXKBqTZV978dVrz4ueNh9d4ULKyfF\""
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "056b4996",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
